238de330270f9500f0bbbb637c69db0afc34ddfa
Pawel Gondek <Pawel.Gondek@sabre.com>
2017-05-04 01:00:30 +0200

    Change Client name Add client name to chat message.

--- src/main/java/com/hello/GreetingController.java
+++ src/main/java/com/hello/GreetingController.java
@@ -1,20 +1,23 @@
 package com.hello;
 
 import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.stereotype.Controller;
 
 /**
  * Created by pawel on 04.03.17.
  */
 @Controller
-public class GreetingController {
-
+public class GreetingController
+{
 
     @MessageMapping("/hello")
     @SendTo("/topic/greetings")
-    public Greeting greeting(HelloMessage message) throws Exception {
-        return new Greeting("Hello, " + message.getName() + "!");
+    public Greeting greeting(@Payload HelloMessage message, SimpMessageHeaderAccessor headerAccessor) throws Exception
+    {
+        return new Greeting(headerAccessor.getUser().getName() + ": " + message.getName());
     }
 
 }
